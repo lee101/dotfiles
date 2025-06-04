4048c91c55298330bd3082f509e814fbdd3b35bb
Lee Penkman <leepenkman@microsoft.com>
2025-06-04 16:24:21 +1200

    wip

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -17,10 +17,30 @@ vim.opt.rtp:prepend(lazypath)
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-require('user.options') -- Load custom options
-require('user.keymaps') -- Load custom keymaps
-require('user.autocommands') -- Load custom autocommands
-require('user.commands') -- Load custom commands
+-- Vim options from vimrc
+vim.opt.number = true
+vim.opt.showcmd = true
+vim.opt.autoindent = true
+vim.opt.expandtab = true
+vim.opt.smarttab = true
+vim.opt.shiftwidth = 3
+vim.opt.softtabstop = 3
+vim.opt.wildmenu = true
+vim.opt.wildmode = "list:longest,full"
+vim.opt.mouse = "a"
+vim.opt.backspace = "2"
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.incsearch = true
+vim.opt.hlsearch = true
+vim.opt.hidden = false
+vim.opt.laststatus = 2
+vim.opt.foldmethod = "marker"
+
+-- require('user.options') -- Load custom options
+-- require('user.keymaps') -- Load custom keymaps
+-- require('user.autocommands') -- Load custom autocommands
+-- require('user.commands') -- Load custom commands
 
 require("lazy").setup({
   spec = {
@@ -29,14 +49,51 @@ require("lazy").setup({
       event = "VeryLazy",
       lazy = false,
       version = false,
-      opts = {},
+      opts = {
+        provider = "copilot",
+        auto_suggestions = true,
+        copilot = {
+          endpoint = "https://api.githubcopilot.com",
+          model = "gpt-4o-2024-05-13",
+          proxy = nil,
+          allow_insecure = false,
+          timeout = 30000,
+        },
+        behaviour = {
+          auto_suggestions = true,
+          auto_set_highlight_group = true,
+          auto_set_keymaps = true,
+          auto_apply_diff_after_generation = false,
+          support_paste_from_clipboard = false,
+        },
+        mappings = {
+          ask = "<leader>aa",
+          edit = "<leader>ae",
+          refresh = "<leader>ar",
+          diff = {
+            ours = "co",
+            theirs = "ct",
+            all_theirs = "ca",
+            both = "cb",
+            cursor = "cc",
+            next = "]x",
+            prev = "[x",
+          },
+          suggestion = {
+            accept = "<M-l>",
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+      },
       build = "make",
       dependencies = {
         "stevearc/dressing.nvim",
         "nvim-lua/plenary.nvim",
         "MunifTanjim/nui.nvim",
         "hrsh7th/nvim-cmp",
-        "zbirenbaum/copilot.lua",
+        "github/copilot.vim",
         {
           "HakonHarnes/img-clip.nvim",
           event = "VeryLazy",
@@ -182,7 +239,80 @@ require("lazy").setup({
         require("numb").setup()
       end
     },
+    {
+      "github/copilot.vim",
+      event = "InsertEnter",
+      config = function()
+        -- Disable default tab mapping
+        vim.g.copilot_no_tab_map = true
+        vim.g.copilot_assume_mapped = true
+        
+        -- Simple reliable mappings
+        vim.api.nvim_create_autocmd("VimEnter", {
+          callback = function()
+            vim.keymap.set("i", "<Tab>", 'copilot#Accept("\\<CR>")', {
+              expr = true,
+              replace_keycodes = false,
+              silent = true
+            })
+            vim.keymap.set("i", "<C-J>", 'copilot#Accept("\\<CR>")', {
+              expr = true,
+              replace_keycodes = false,
+              silent = true
+            })
+          end,
+        })
+      end,
+    },
+    {
+      "hrsh7th/nvim-cmp",
+      event = "InsertEnter",
+      dependencies = {
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "hrsh7th/cmp-nvim-lsp",
+      },
+      config = function()
+        local cmp = require("cmp")
+        cmp.setup({
+          mapping = cmp.mapping.preset.insert({
+            ["<Tab>"] = cmp.mapping.select_next_item(),
+            ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+            ["<CR>"] = cmp.mapping.confirm({ select = true }),
+            ["<C-Space>"] = cmp.mapping.complete(),
+          }),
+          sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "buffer" },
+            { name = "path" },
+          }),
+        })
+      end,
+    },
   },
   install = { colorscheme = { "tokyonight" } },
-  checker = { enabled = true },
+  checker = { enabled = false },
+  change_detection = { enabled = false },
 })
+
+-- Key mappings from vimrc (set after plugins to avoid conflicts)
+vim.keymap.set("i", "jj", "<Esc>", { desc = "Exit insert mode with jj" })
+vim.keymap.set("n", ";", ":", { desc = "Use semicolon for command mode" })
+vim.keymap.set("n", ":", ";", { desc = "Use colon for repeat find" })
+
+-- Use a different key for fold toggle since space is leader
+vim.keymap.set("n", "za", "za", { desc = "Toggle fold" })
+
+vim.keymap.set("n", "k", "gk", { desc = "Move up by display line" })
+vim.keymap.set("n", "j", "gj", { desc = "Move down by display line" })
+vim.keymap.set("n", "n", "nzz", { desc = "Next search result centered" })
+vim.keymap.set("n", "N", "Nzz", { desc = "Previous search result centered" })
+
+-- Tab navigation
+vim.keymap.set("n", "<C-Right>", ":tabnext<CR>", { desc = "Next tab" })
+vim.keymap.set("n", "<C-Left>", ":tabprevious<CR>", { desc = "Previous tab" })
+vim.keymap.set("n", "<C-t>", ":tabnew<CR>", { desc = "New tab" })
+
+-- Create blank lines
+vim.keymap.set("n", "zj", "o<Esc>", { desc = "Create blank line below" })
+vim.keymap.set("n", "zk", "O<Esc>", { desc = "Create blank line above" })
